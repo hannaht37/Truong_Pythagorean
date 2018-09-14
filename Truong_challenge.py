72a3529ac506c2dca243a6b17f6642f7ecd43758
Hannah Truong <hannahtru.57352@redlandsschools.net>
2018-09-14 15:25:39 +0000

    triangles

--- Truong_challenge.py
+++ Truong_challenge.py
@@ -1,3 +1,17 @@
 """
 Write a program that will determine if the user's input is a triangle and what type of triangle
 """
+print("Hi there! Today we will be determining if the three lengths you will enter are triangles and what type of triangle it is. ")
+greeting = input("What is your name? ")
+print("Nice to meet you, " + greeting + ". Let's get started.")
+a = float(input("First, please enter a length for leg a : "))
+b = float(input("Now please enter a length for leg b : "))
+c = float(input("Finally, please enter a length for leg c : "))
+if(a+b<c):
+    print("Oh No! It looks like the lengths you entered is not a triangle.")
+if(a**2 + b**2 == c**2):
+    print("Congrats! The values you entered are equal to a right triangle!")
+if(a**2 + b**2 > c**2):
+    print("Yay, the lengths you entered are equal to an acute angle!")
+elif(a**2 + b**2 < c**2):
+    print("The lengths you entered are equal to an obtuse angle!")
