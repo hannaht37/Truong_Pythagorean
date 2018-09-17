ef78f736f28d7dd72711e594af5b180395edfc01
Hannah Truong <hannahtru.57352@redlandsschools.net>
2018-09-17 00:02:18 +0000

    final

--- Truong_challenge.py
+++ Truong_challenge.py
@@ -9,9 +9,10 @@ b = float(input("Now please enter a length for leg b : "))
 c = float(input("Finally, please enter a length for leg c : "))
 if(a+b<c):
     print("Oh No! It looks like the lengths you entered is not a triangle.")
-if(a**2 + b**2 == c**2):
-    print("Congrats! The values you entered are equal to a right triangle!")
-if(a**2 + b**2 > c**2):
-    print("Yay, the lengths you entered are equal to an acute angle!")
-elif(a**2 + b**2 < c**2):
-    print("The lengths you entered are equal to an obtuse angle!")
+else:
+    if(a**2 + b**2 == c**2):
+        print("Congrats! The values you entered are equal to a right triangle!")
+    if(a**2 + b**2 > c**2):
+        print("Yay, the lengths you entered are equal to an acute angle!")
+    elif(a**2 + b**2 < c**2):
+        print("The lengths you entered are equal to an obtuse angle!")  
